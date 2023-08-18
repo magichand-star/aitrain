3707691f148a98b8a9c8940e8e4b567b75604944
magichand-star <robust99213@gmail.com>
2023-08-18 19:56:51 +0300

    com]

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ os.environ["OPENAI_API_KEY"] = 'sk-QD4hLruyKC378ic4Gj4OT3BlbkFJrTjINLbbCJ3cq2sGe
 def construct_index(directory_path):
     max_input_size = 4096
     num_outputs = 512
-    max_chunk_overlap = 20
+    max_chunk_overlap = 20 
     chunk_size_limit = 600
 
     prompt_helper = PromptHelper(
